2ec0d279683c72d8749ed3756e4c45bc30bc307a
abimainurriza <aabimainurriza@gmail.com>
2024-09-11 14:21:47 +0700

    Praktikum 5A - Memisahkan Class

---
+++ Book.php
@@ -0,0 +1,49 @@
+<?php 
+class Book {
+     public $ISBN;
+     public $title;
+     public $description;
+     public $category;
+     public $language;
+     public $numberOfPage;
+     public $author;
+     public $publisher;
+ 
+     public function __construct($ISBN, $title, $description, $category, $language, $numberOfPage, $author, $publisher) {
+         $this->ISBN = $ISBN;
+         $this->title = $title;
+         $this->description = $description;
+         $this->category = $category;
+         $this->language = $language;
+         $this->numberOfPage = $numberOfPage;
+         $this->author = $author;
+         $this->publisher = $publisher;
+     }
+ 
+     public function showAll() {
+         $detail = [
+             "ISBN" => $this->ISBN,
+             "Title" => $this->title,
+             "Description" => $this->description,
+             "Category" => $this->category,
+             "Language" => $this->language,
+             "Number of Pages" => $this->numberOfPage,
+             "Author" => $this->author->name,
+             "Publisher" => $this->publisher->name
+         ];
+ 
+         $result = "";
+         foreach ($detail as $key => $value) {
+             $result .= "$key: $value\n";
+         }
+         return $result;
+     }
+ 
+     public function detail($ISBN) {
+         if ($this->ISBN == $ISBN) {
+             return $this->showAll();
+         } else {
+             return "Sorry, Book not found sir";
+         }
+     }
+ }
---
+++ Publisher.php
@@ -0,0 +1,21 @@
+<?php
+
+class Publisher {
+     public $name;
+     public $address;
+     public $phone;
+ 
+     public function __construct($name, $address, $phone) {
+         $this->name = $name;
+         $this->address = $address;
+         $this->phone = $phone;
+     }
+ 
+     public function setPhone($phone) : void {
+         $this->phone = $phone;
+     }
+ 
+     public function getPhone() : int {
+         return $this->phone;
+     }
+ }
