927f4ad311741969867c7bb5589c8f5be444bdac
abimainurriza <aabimainurriza@gmail.com>
2024-09-23 18:00:32 +0700

    Add files via upload

---
+++ App/sepuluh.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require_once 'App/Admin/HomePage.php';
+require_once 'App/Admin/ManajemenArtikel.php';
+
+
+use App\Admin\HomePage;
+use App\Admin\ManajemenArtikel;
+
+
+$hp = new HomePage();
+$hp->tampil();
+
+echo "\n";
+
+$ma = new ManajemenArtikel();
+$ma->tambah();
