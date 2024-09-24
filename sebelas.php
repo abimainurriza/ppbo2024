ce1a7645d33753c2eefb6b279f14a66af33f6f43
abimainurriza <aabimainurriza@gmail.com>
2024-09-24 21:05:43 +0700

    Delete sebelas.php

--- sebelas.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require_once 'vendor/autoload.php';
-
-use App\Admin\Dosen;
-
-$dian = new Dosen(198411132015041001, "Dian Prawira", "Jln.Purnama", 62111111, "0013118405");
-$dian->mengajar();
